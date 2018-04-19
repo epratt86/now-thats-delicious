9671d820a5290b0e1515564d91c4428ca202c4a3
Eric Pratt <ericpratt86@gmail.com>
2018-04-19 12:54:47 -0700

    map api

--- routes/index.js
+++ routes/index.js
@@ -52,9 +52,12 @@ router.post('/account/reset/:token',
   authController.confirmedPasswords,
   catchErrors(authController.update)
 );
+router.get('/map', storeController.mapPage);
 
 /*
   API ROUTES
 */
 router.get('/api/search', catchErrors(storeController.searchStores));
+router.get('/api/stores/near', catchErrors(storeController.mapStores));
+
 module.exports = router;
